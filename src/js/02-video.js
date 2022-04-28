f34ede65b46a1ac08f58ec84c499ae6d5541f6d6
Andrii Berezhko <Berezhko7777@ukr.net>
2022-04-28 20:44:39 +0300

    2 task
    половина

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+
+const player = new Player('vimeo-player', {
+    // id: "vimeo-player",
+    width: 640
+});
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+const videoData = function (data) {
+    console.log(data);
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+};
+player.on('timeupdate', videoData);
 
