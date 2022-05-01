fa797b7959aa94c233f52f7531bb7e6bf4163bf5
Andrii Berezhko <Berezhko7777@ukr.net>
2022-05-01 13:23:45 +0300

    2 task
    done 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,38 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const player = new Player('vimeo-player', {
-    // id: "vimeo-player",
+    
     width: 640
 });
 
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
-const videoData = function (data) {
-    console.log(data);
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
-};
-player.on('timeupdate', videoData);
 
+player.setCurrentTime(savedCurentTime()).then(function (seconds) {
+    
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+player.on('timeupdate', throttle(function (data) {
+   
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+},1000));
+
+
+function savedCurentTime() {
+    const savedTime = localStorage.getItem("videoplayer-current-time");
+    if (savedTime) {
+        console.log(savedTime); 
+        return savedTime;  
+    };
+    
+};
