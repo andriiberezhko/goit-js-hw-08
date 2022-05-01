8d82cd5577977f8895487748b3d08e580a272e1e
Andrii Berezhko <Berezhko7777@ukr.net>
2022-05-01 15:38:14 +0300

    3 task
    Done 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
 
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = "feedback-form-state";
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    
+};
+const formData = {};
+
+populateInputData();
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+   
+    formData[event.target.name] = event.target.value;
+    
+  
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log( JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    event.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function populateInputData() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+   
+    if (savedData) {
+        if (savedData.message != undefined) {
+            refs.form.message.value = savedData.message;
+            formData.message = savedData.message;
+        };
+
+        if (savedData.email != undefined) {
+            refs.form.email.value = savedData.email; 
+            formData.email = savedData.email;
+        };
+        
+         
+    };
+   
+};
